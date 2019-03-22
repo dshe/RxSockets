348686e561779a474d84c9a8251bd0ae2c8ff537
DavidS <david@tan.com>
2019-03-22 14:03:21 +0800

    Net Core 2.1 testing

--- RxSockets.Tests/RxSocketServerTests.cs
+++ RxSockets.Tests/RxSocketServerTests.cs
@@ -48,8 +48,8 @@ namespace RxSockets.Tests
         {
             var server = RxSocketServer.Create(EndPoint);
             await server.DisconnectAsync();
-            await Task.Yield();
-            await server.AcceptObservable.LastOrDefaultAsync();
+            //await Task.Yield();
+            //await server.AcceptObservable.LastOrDefaultAsync();
         }
 
         [Fact]
